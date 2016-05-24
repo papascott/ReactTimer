31f02d28b9e09486c402017d3d680f9253fe4ee5
papascott <papascott@gmail.com>
2016-05-24 21:56:57 +0200

    Add CountdownForm component and tests

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,9 +4,8 @@ var Navigation = require('Navigation');
 var Main = (props) =>
   <div>
     <Navigation/>
-    <div>
-      <div>
-        <p>Main.jsx rendered</p>
+    <div className="row">
+      <div className="column small-centered medium-6 large-4">
         {props.children}
       </div>
     </div>
--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,8 +1,6 @@
 var React = require('react');
 var Clock = require('Clock');
 
-var Timer = () =>
-  <div><Clock totalSeconds={129}/></div>
-  ;
+var Timer = () => <div>Timer.jsx</div>;
 
 module.exports = Timer;
